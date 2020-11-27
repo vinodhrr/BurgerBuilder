2b8c55fae08baef8d01dcf37c50aaa667cb41c20
Vinodh Rr <rrvinodh765@gmail.com>
2020-11-27 17:12:00 +0530

    adding BackDrop

---
+++ src/components/UI/BackDrop/BackDrop.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import classes from './BackDrop.module.css'
+
+const BackDrop = props => (
+    props.show ? <div  
+                  className={classes.BackDrop}
+                  onClick={props.clicked}
+                  ></div> : null
+);
+
+export default BackDrop
