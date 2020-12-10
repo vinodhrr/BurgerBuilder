536c930eaccf4a4fd726b4c6f3682185c58e34cb
Vinodh Rr <rrvinodh765@gmail.com>
2020-12-10 21:51:50 +0530

    added logo in sidedrawer in png format

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import BurgerLogo from '../../assets/images/burger-logo.png'
+// import BurgerLogo from '../../assets/images/burger-logo.png'
+import BurgerLogo from '../../assets/images/burger-logo1.png'
 import * as classes from './Logo.module.css'
 
 const Logo = props => (
